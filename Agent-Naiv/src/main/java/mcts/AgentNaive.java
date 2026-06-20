0cd257877f5b53a04b7b2b3e367ec219ac3654f2
Maximilian Fischer <54480672+Maxi1324@users.noreply.github.com>
2026-06-20 15:47:29 +0200

    refactor: clean up constructor and factory overrides in AgentNaive

--- Agent-Naiv/src/main/java/mcts/AgentNaive.java
+++ Agent-Naiv/src/main/java/mcts/AgentNaive.java
@@ -11,18 +11,28 @@ import java.util.Random;
 public class AgentNaive extends AbstractDeterminizedAgent<AgentNaive.NaiveNode> {
     private static final AbstractAgentConfiguration CONFIG = new AbstractAgentConfiguration(4f, 5, 0.1f, 5);
 
+    public AgentNaive(Logger logger) {
+        this(logger, new Random(99));
+    }
+
     public static final class NaiveNode extends DeterminizedActionNode<NaiveNode> {
         NaiveNode(AgentNaive agent, NaiveNode parent, int action, State checkpoint) {
             super(agent, parent, action, checkpoint);
         }
     }
 
-    public AgentNaive() {
-        this(null, new Random(99));
+    @Override
+    protected NaiveNode rootFactory(State initialState) {
+        return new NaiveNode(this, null, 0, initialState);
     }
 
-    public AgentNaive(Logger logger) {
-        this(logger, new Random(99));
+    @Override
+    public NaiveNode childFactory(NaiveNode parent, int action, State checkpoint) {
+        return new NaiveNode(this, parent, action, checkpoint);
+    }
+
+    public AgentNaive() {
+        this(null, new Random(99));
     }
 
     public AgentNaive(Random rand) {
@@ -38,13 +48,4 @@ public class AgentNaive extends AbstractDeterminizedAgent<AgentNaive.NaiveNode> 
         super(logger, config, rand);
     }
 
-    @Override
-    protected NaiveNode rootFactory(State initialState) {
-        return new NaiveNode(this, null, 0, initialState);
-    }
-
-    @Override
-    public NaiveNode childFactory(NaiveNode parent, int action, State checkpoint) {
-        return new NaiveNode(this, parent, action, checkpoint);
-    }
 }
